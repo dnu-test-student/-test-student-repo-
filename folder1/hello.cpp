33a8f957aa37e9b6aa4c531286f86cae0526da98
dnu-test-student <42960237+dnu-test-student@users.noreply.github.com>
2018-10-23 16:40:20 +0300

    Update hello.cpp
    Исправил код в ответ на замечания Ятагана ( ! )

--- folder1/hello.cpp
+++ folder1/hello.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
-int main()
+void main()
 {
-   cout << "Hello, World! :)" << endl;
-   return 0;
+   cout << "Hello, World! :);)" << endl;
+   //return 0;
 }
